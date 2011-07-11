fdc087385b86b3f6dc8f6ceb19a42da5d2d1c602
Dave Dash <dd+github@davedash.com>
2011-07-10 21:10:40 -0700

    An ES mapping for opinion

--- apps/search/cron.py
+++ apps/search/cron.py
@@ -1,7 +1,11 @@
+from django.conf import settings
+
 import commonware.log
 import cronjobs
+import pyes.exceptions as pyes
 from celery.messaging import establish_connection
 from celeryutils import chunked
+from elasticutils import get_es
 
 import input
 from feedback.models import Opinion
@@ -22,3 +26,16 @@ def index_all():
     with establish_connection() as conn:
         for chunk in chunked(ids, 1000):
             tasks.add_to_index.apply_async(args=[chunk], connection=conn)
+
+
+@cronjobs.register
+def setup_mapping():
+    m = dict(
+            product=dict(type='long', ),
+            )
+    es = get_es()
+    try:
+        es.create_index_if_missing(settings.ES_INDEX)
+        es.put_mapping('opinion', {'properties': m}, settings.ES_INDEX)
+    except pyes.ElasticSearchException as e:
+        log.debug(e)
